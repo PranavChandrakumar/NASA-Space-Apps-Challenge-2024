d92937f278e6447a6a51a461a330c1d1f4b9b8f3
HashBukhtiar <hashbukhtiar@gmail.com>
2024-10-05 17:22:51 -0400

    Changed Navbar to have title of HELIOS and caption

--- helios-frontend/src/components/Navbar.tsx
+++ helios-frontend/src/components/Navbar.tsx
@@ -8,13 +8,10 @@ const Navbar: React.FC = () => {
   return (
     <nav className='sticky top-0 z-50 h-20 w-full flex items-center justify-between p-5 bg-slate-100/75 backdrop-blur-sm'>
       <div className='flex items-center'>
-        <div>this is a Navbar</div>
-        <div className='w-full flex items-center gap-x-3'>
-          <Link href='/'>Home</Link>
-          <Link href='/'>Foo</Link>
-          <Link href='/'>Bar</Link>
+        <div>HELIOS.</div>
+        <div className='w-full flex items-center gap-x-3' style={{marginLeft: '20px'}}>
+          <p>Want to detect surface reflectance?</p>
         </div>
-        <Button>Test</Button>
       </div>
     </nav>
   );
