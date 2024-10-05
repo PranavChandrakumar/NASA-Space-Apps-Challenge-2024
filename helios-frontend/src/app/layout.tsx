fa0aca7cd55c26372020b41cff076fcd6e9ad0db
Marco Tan <marco@ionicargon.ca>
2024-10-05 12:25:12 -0400

    Configure shadcn

--- helios-frontend/src/app/layout.tsx
+++ helios-frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
-import Navbar from '@components/Navbar';
-import Footer from '@components/Footer';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Helios',
