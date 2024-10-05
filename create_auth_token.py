ce9e9898d68ae63edb90f600d112d6ea9af0c568
Pranav Chandrakumar <pranav.k.chandrakumar@gmail.com>
2024-10-05 15:19:16 -0400

    token creation

--- create_auth_token.py
+++ create_auth_token.py
@@ -1,9 +1,21 @@
 import requests
 
-def GetToken():
-    response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', auth=('heliosbe', 'NASASpaceApps2024!'))
+
+ 
+def SetToken(): #Call this to set a token
+    response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', auth=('helios_maplenauts', 'NASASpaceApp2024!'))
     token_response = response.json()
-    print(token_response)
-    
-    return token_response
-    
+
+    TOKEN = token_response['token']
+    TOKEN = TOKEN 
+
+    with open('token.txt', 'w') as file:
+        file.write(TOKEN)
+
+    return
+
+def GetToken(): #Call this to get the token
+    with open('token.txt', 'r') as file:
+         TOKEN = file.readline().strip()
+
+    return TOKEN  # Return the TOKEN variable
