8b1f315a0d3f5b9f2d769538ea6bba75b16d700c
HashBukhtiar <hashbukhtiar@gmail.com>
2024-10-05 14:12:22 -0400

    Changed background colors

--- helios-frontend/src/components/Footer.tsx
+++ helios-frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className='bg-gray-800 text-white py-4'>
+    <footer className='bg-gray-800 text-white py-4' style={{ backgroundColor: '#1C1B29', borderTopWidth:1 , borderTopColor: 'white'}}>
       <div className='container mx-auto text-center'>
         <p>&copy; {new Date().getFullYear()} Helios. All rights reserved.</p>
         <p>Andrew De Rango | Hashim Bukhtiar | Hashir Imam | Marco Tan | Pranav Chandrakumar | Rafael Toameh</p>
