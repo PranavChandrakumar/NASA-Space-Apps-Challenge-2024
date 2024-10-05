960d64849842dabbe17092b1bc3b750e2da61d0e
Pranav Chandrakumar <pranav.k.chandrakumar@gmail.com>
2024-10-05 14:01:13 -0400

    test

--- create_auth_token.py
+++ create_auth_token.py
@@ -1,10 +1,5 @@
 import requests
 
-
-response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', auth=('heliosbe', 'NASASpaceApps2024!'))
-token_response = response.json()
-print(token_response)
-
 def GetToken():
     response = requests.post('https://appeears.earthdatacloud.nasa.gov/api/login', auth=('heliosbe', 'NASASpaceApps2024!'))
     token_response = response.json()
