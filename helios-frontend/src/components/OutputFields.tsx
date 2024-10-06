04ab1b6a5fbd4cf767129dd0907f40461ed5169f
Hashcash-bit <hashirimam2004@gmail.com>
2024-10-06 17:07:47 -0400

    Update: Helios || Gotta make the year, time and stuff

--- helios-frontend/src/components/OutputFields.tsx
+++ helios-frontend/src/components/OutputFields.tsx
@@ -1,21 +1,41 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+import gameIcon from '../app/game-icons_hot-surface.png';
+import iCtwotone from '../app/ic_twotone-hd.png';
+import satellite1 from '../app/openmoji_satellite-1.png';
+import satellite from '../app/openmoji_satellite.png';
+import solarCloud from '../app/solar_clouds-bold.png';
+import solarTemp from '../app/solar_temperature-bold-duotone.png';
 
 const OutputFields: React.FC = () => {
   const [outputs, setOutputs] = useState<{ label: string; value: string }[]>([
-    { label: 'Cloud Coverage', value: '' },
-    { label: 'Landsat 8 Next Overpass', value: '{DATETIME}' },
-    { label: 'Landsat 9 Next Overpass', value: '{DATETIME}' },
-    { label: 'Image Quality Rating', value: '{RATING}' },
-    { label: 'Surface Reflectance', value: '{REFLECTANCE}' },
-    { label: 'Surface Temperature', value: '{TEMPERATURE} degrees C/F' },
+    { label: 'Cloud Coverage', value: '', image: solarCloud },
+    { label: 'Landsat 8 Next Overpass', value: '{DATETIME}', image: satellite },
+    {
+      label: 'Landsat 9 Next Overpass',
+      value: '{DATETIME}',
+      image: satellite1,
+    },
+    { label: 'Image Quality Rating', value: '{RATING}', image: iCtwotone },
+    { label: 'Surface Reflectance', value: '{REFLECTANCE}', image: gameIcon },
+    {
+      label: 'Surface Temperature',
+      value: '{TEMPERATURE} degrees C/F',
+      image: solarTemp,
+    },
   ]);
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const cloudCoverageOptions = ['No Preference', 'Low Coverage', 'Up to Medium Coverage', 'All Coverage Data'];
+  const cloudCoverageOptions = [
+    'No Preference',
+    'Low Coverage',
+    'Up to Medium Coverage',
+    'All Coverage Data',
+  ];
 
   const handleInputChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newValue = event.target.value;
@@ -124,7 +144,7 @@ const OutputFields: React.FC = () => {
             padding: '10px 20px',
             borderRadius: 8,
             backgroundColor: '#f0f0f0',
-            marginLeft: 10
+            marginLeft: 10,
           }}
         />
       </div>
@@ -162,31 +182,66 @@ const OutputFields: React.FC = () => {
         </button>
       </div>
       {outputs.map((output, index) => (
-        <div key={index} style={{}}>
-          {output.label === 'Cloud Coverage' ? (
-            <>
-              {output.label}:{' '}
-              <select
-                value={output.value}
-                onChange={handleInputChange}
-                style={{
-                  width: '200px',
-                  textAlign: 'right',
-                  backgroundColor: '#f0f0f0',
-                  borderRadius: 8,
-                  marginLeft: 10,
-                }}
-              >
-                {cloudCoverageOptions.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            </>
-          ) : (
-            `${output.label}: ${output.value}`
-          )}
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: '10px',
+            width: '100%',
+          }}
+        >
+          {/* Image for the output */}
+          <Image
+            src={output.image}
+            alt={output.label}
+            style={{ width: '40px', height: '40px', marginRight: '10px' }}
+          />
+
+          {/* Divs for label and value */}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              flex: 1,
+              justifyContent: 'space-between',
+              alignContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <div
+              style={{
+                fontWeight: 'bold',
+                color: '#EDE9D5',
+                marginBottom: '5px',
+              }}
+            >
+              {output.label}:
+            </div>
+            <div style={{ color: '#6D6875', fontSize: '0.9em' }}>
+              {output.label === 'Cloud Coverage' ? (
+                <select
+                  value={output.value}
+                  onChange={handleInputChange}
+                  style={{
+                    width: '200px',
+                    textAlign: 'right',
+                    backgroundColor: '#f0f0f0',
+                    borderRadius: 8,
+                    padding: '10px',
+                  }}
+                >
+                  {cloudCoverageOptions.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                `${output.value}`
+              )}
+            </div>
+          </div>
         </div>
       ))}
     </div>
